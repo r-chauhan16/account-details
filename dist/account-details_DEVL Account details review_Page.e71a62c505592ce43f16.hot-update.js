28886a2db6ab83c4e9711da4efc70f3d973bdbad
Ravi Chauhan <4301942+r-chauhan16@users.noreply.github.com>
2024-03-14 03:25:42 +0530

    Add files via upload

---
+++ dist/account-details_DEVL Account details review_Page.e71a62c505592ce43f16.hot-update.js
@@ -0,0 +1,18 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateaccount_details_extension"]("account-details_DEVL Account details review_Page",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("909cff5adec3fdd7d170")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ src/page/AccountDetails.jsx
@@ -0,0 +1,376 @@
+// Copyright 2021-2023 Ellucian Company L.P. and its affiliates.
+
+import React, { useEffect, useMemo, useState } from 'react';
+import { useIntl } from 'react-intl';
+import classenames from 'classnames';
+
+import {
+    Button,
+    makeStyles,
+    Pagination,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableRow,
+    Typography
+} from '@ellucian/react-design-system/core'
+import { colorFillAlertError, colorTextAlertSuccess, spacing30, spacing40, widthFluid } from '@ellucian/react-design-system/core/styles/tokens';
+
+import { useCardInfo, useExtensionControl, useUserInfo } from '@ellucian/experience-extension-utils';
+
+import { DataQueryProvider, experienceTokenQuery, useDataQuery } from '@ellucian/experience-extension-extras';
+
+// initialize logging for this card
+import { initializeLogging } from '../util/log-level';
+initializeLogging('default');
+
+const featurePayNow = process.env.FEATURE_PAY_NOW === 'true';
+
+const resource = 'account-detail-reviews';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        height: '100%',
+        overflowY: 'auto'
+    },
+    content: {
+        height: '100%',
+        marginTop: 0,
+        marginRight: spacing40,
+        marginBottom: 0,
+        marginLeft: spacing40,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    transactionPaper: {
+        width: widthFluid
+    },
+    transactionsTableBox: {
+        marginBottom: spacing30,
+        overflowX: 'auto'
+    },
+    transactionsBox: {
+    },
+    recentTransactions: {
+        marginBottom: spacing30
+    },
+    transactionsTable: {
+        minWidth: 500
+    },
+    transactionAmountPayment: {
+        color: colorTextAlertSuccess
+    },
+    amountBoxRow: {
+        marginTop: spacing40,
+        marginBottom: spacing40,
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    amountBox: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    amountRow: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    amount: {
+        marginLeft: spacing30
+    },
+    payNowButton: {
+        marginLeft: spacing30
+    }
+}), { index: 2 });
+
+function AccountDetails() {
+    const intl = useIntl();
+    const classes = useStyles();
+
+    // Experience SDK hooks
+    const { configuration, cardConfiguration } = useCardInfo();
+    const { setErrorMessage, setLoadingStatus } = useExtensionControl();
+    const { roles, locale } = useUserInfo();
+
+    const { payNowUrl } = configuration || cardConfiguration || {};
+
+    const { data, isError, isLoading, isRefreshing } = useDataQuery(resource);
+
+    const [transactions, setTransactions] = useState([]);
+    const [summary, setSummary] = useState();
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, transactions.length - page * rowsPerPage);
+
+    const [dateFormater, setDateFormater] = useState();
+    const [currentyFormater, setCurrentyFormater] = useState();
+
+    // for pending aids
+    const erpId = roles[roles.length - 2];
+    const [pdata, setpData] = useState([]);
+    const [pdataFetched, setpDataFetched] = useState(false);
+
+    // console.log(pdata);
+    useEffect(() => {
+        const fetchpData = async () => {
+            try {
+                const customApi = `https://oradevl.lasierra.edu:9443/api/pendingaid?erpid=${erpId}`;
+                // const customApi = `https://oradevl.lasierra.edu:9443/api/pendingaid?erpid=${erpId}`;
+                console.log('API_URL:', customApi);
+                const response = await fetch(customApi);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const jsonData = await response.json();
+                setpData(jsonData);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+        if (!pdataFetched) {
+            fetchpData();
+        }
+    }, [erpId, pdataFetched, setpDataFetched]);
+
+    // set up formaters with user's locale
+    useEffect(() => {
+        if (locale) {
+            setDateFormater(new Intl.DateTimeFormat(locale, { year: 'numeric', month: '2-digit', day: '2-digit' }));
+            setCurrentyFormater(new Intl.NumberFormat(locale, { style: 'currency', currency: 'USD' }))
+        }
+    }, [locale])
+
+    useEffect(() => {
+        setLoadingStatus(isRefreshing || (!data && isLoading));
+    }, [data, isLoading, isRefreshing])
+
+    useEffect(() => {
+        if (data) {
+            const [results] = data;
+            // console.log("I am here.......................")
+            // console.log(results);
+            let transactions = [];
+            let summarys = [{
+                accountBalance: 0,
+                amountDue: 0
+            }];
+
+            if (results) {
+                ({ TBRACCD: transactions, TBRACCD_CTRL: summarys } = results);
+                transactions.sort((left, right) => (right.transDate?.localeCompare(left.transDate)));
+            }
+
+            setTransactions(transactions);
+            setSummary(() => summarys[0]);
+        }
+    }, [data])
+
+    useEffect(() => {
+        if (isError) {
+            setErrorMessage({
+                headerMessage: intl.formatMessage({ id: 'AccountDetails.contactAdministrator' }),
+                textMessage: intl.formatMessage({ id: 'AccountDetails.dataError' }),
+                iconName: 'warning',
+                iconColor: colorFillAlertError
+            });
+        }
+    }, [isError, setErrorMessage])
+
+    if (!data) {
+        // nothing to show yet
+        return null;
+    }
+
+    function handleChangePage(event, page) {
+        setPage(page);
+    }
+
+    function handleChangeRowsPerPage(event) {
+        setRowsPerPage(event.target.value)
+    }
+
+    function onPayNow() {
+        if (payNowUrl) {
+            window.open(payNowUrl, '_blank');
+        }
+    }
+
+    const showPayNow = featurePayNow && payNowUrl && summary?.accountBalance > 0;
+    const firstDate = Array.isArray(transactions) && transactions.length > 0 && transactions[0].transDate ? dateFormater.format((new Date(transactions[0]?.transDate) || Date.now)) : '';
+    const lastDate = Array.isArray(transactions) && transactions.length > 0 && transactions[transactions.length - 1].transDate ? dateFormater.format(new Date(transactions[transactions.length - 1].transDate)) : '';
+    return (
+        <div className={classes.root}>
+            <div className={classes.content}>
+                <>
+                    {summary && (
+                        <div className={classes.amountBoxRow}>
+                            <div className={classes.amountBox}>
+                                <div className={classes.amountRow}>
+                                    <Typography variant={'h4'} component={'div'}>
+                                        {intl.formatMessage({ id: 'AccountDetails.accountBalance' })}
+                                    </Typography>
+                                    <Typography variant={'body2'} component={'div'} className={classes.amount}>
+                                        {currentyFormater.format(summary.accountBalance)}
+                                    </Typography>
+                                </div>
+                                <div className={classes.amountRow}>
+                                    <Typography variant={'h4'} component={'div'}>
+                                        {intl.formatMessage({ id: 'AccountDetails.amountDue' })}
+                                    </Typography>
+                                    <Typography variant={'body2'} component={'div'} className={classes.amount}>
+                                        {currentyFormater.format(summary.amountDue)}
+                                    </Typography>
+                                </div>
+                                <div className={classes.amountRow}>
+                                    <Typography variant={'h4'} component={'div'}>
+                                        {intl.formatMessage({ id: 'PendingAids' })}
+                                    </Typography>
+                                    <Typography variant={'body2'} component={'div'} className={classes.amount}>
+                                        {pdata.map((innerArray, index) => (
+                                            innerArray && (
+                                                <div key={index}>
+                                                    -{currentyFormater.format(innerArray[7] + innerArray[8])}
+                                                </div>
+                                            )
+                                        ))}
+                                    </Typography>
+                                </div>
+                                <div className={classes.amountRow}>
+                                    <Typography variant={'h4'} component={'div'}>
+                                        {intl.formatMessage({ id: 'yourResponsiblity' })}
+                                    </Typography>
+                                    <Typography variant={'body2'} component={'div'} className={classes.amount}>
+                                        {pdata.map((innerArray, index) => (
+                                            innerArray && (
+                                                <div key={index}>
+                                                    {currentyFormater.format(summary.accountBalance - innerArray[7] - innerArray[8])}
+                                                </div>
+                                            )
+                                        ))}
+                                    </Typography>
+                                </div>
+                            </div>
+                            {(
+                                <Button className={classes.payNowButton} color='secondary' onClick={onPayNow}>
+                                    {intl.formatMessage({ id: 'AccountDetails.payNow' })}
+                                </Button>
+                            )}
+                        </div>
+                    )}
+                    {Array.isArray(transactions) && transactions.length > 0 && (
+                        <div className={classes.transactionsBox}>
+                            <Typography variant={'h4'} component={'div'} className={classes.recentTransactions}>
+                                {intl.formatMessage({ id: 'AccountDetails.transactionHistory' })} {lastDate} - {firstDate}
+                            </Typography>
+                            <Paper className={classes.transactionPaper}>
+                                <div className={classes.transactionsTableBox}>
+                                    <Table className={classes.transactionsTable}>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>{intl.formatMessage({ id: 'AccountDetails.transactionDate' })}</TableCell>
+                                                <TableCell>{intl.formatMessage({ id: 'AccountDetails.description' })}</TableCell>
+                                                <TableCell>{intl.formatMessage({ id: 'AccountDetails.type' })}</TableCell>
+                                                <TableCell align="right">{intl.formatMessage({ id: 'AccountDetails.amount' })}</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {transactions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(transaction => {
+                                                const { chargeAmount, desc, paymentAmount, transDate, tranNumber } = transaction;
+                                                const transactionDate = transDate ? dateFormater.format(new Date(transDate)) : '';
+                                                const type = chargeAmount ? intl.formatMessage({ id: 'AccountDetails.charge' }) : intl.formatMessage({ id: 'AccountDetails.payment' });
+                                                const amount = currentyFormater.format(chargeAmount ? chargeAmount : paymentAmount * -1);
+                                                return (
+                                                    <TableRow key={tranNumber} className={classes.transactionsTableRow}>
+                                                        <TableCell align="left">
+                                                            <Typography variant={'body3'} component={'div'}>
+                                                                {transactionDate}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            <Typography variant={'body3'} component={'div'}>
+                                                                {desc}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            <Typography variant={'body3'} component={'div'}>
+                                                                {type}
+                                                            </Typography>
+                                                        </TableCell>
+                                                        <TableCell align="right">
+                                                            <Typography variant={'body3'} component={'div'} className={classenames({ [classes.transactionAmountPayment]: !chargeAmount })}>
+                                                                {amount}
+                                                            </Typography>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                );
+                                            })}
+                                            {emptyRows > 0 && [...Array(emptyRows)].map((value, index) => {
+                                                return (
+                                                    <TableRow key={index} className={classes.transactionsTableRow}>
+                                                        <TableCell />
+                                                        <TableCell />
+                                                        <TableCell />
+                                                        <TableCell />
+                                                    </TableRow>
+                                                )
+                                            })}
+                                        </TableBody>
+                                        <TableFooter>
+                                            <TableRow>
+                                                <Pagination
+                                                    component="td"
+                                                    count={transactions.length}
+                                                    rowsPerPage={rowsPerPage}
+                                                    rowsPerPageOptions={[10, 20, 50, 100]}
+                                                    page={page}
+                                                    onPageChange={handleChangePage}
+                                                    onRowsPerPageChange={handleChangeRowsPerPage}
+                                                />
+                                            </TableRow>
+                                        </TableFooter>
+                                    </Table>
+                                </div>
+                            </Paper>
+                        </div>
+                    )}
+                </>
+            </div>
+        </div>
+    );
+}
+
+function AccountDetailsWithProviders() {
+    const {
+        cardConfiguration: {
+            serviceUrl
+        } = {}
+    } = useCardInfo();
+    console.log(serviceUrl);
+
+    const aidserviceURL = 'https://rklj2lb5ak.execute-api.us-east-1.amazonaws.com/api';
+    console.log(aidserviceURL);
+
+    const options = useMemo(() => ({
+        queryFunction: experienceTokenQuery,
+        queryParameters: { serviceUrl },
+        resource: 'account-detail-reviews'
+    }));
+
+    // const options2 = useMemo(() => ({
+    //    queryFunction: experienceTokenQuery,
+    //    queryParameters: { serviceUrl : 'https://rklj2lb5ak.execute-api.us-east-1.amazonaws.com/api' },
+    //    resource: 'award-maintenance'
+    // }));
+
+    return (
+        <DataQueryProvider options={options}>
+            <AccountDetails />
+        </DataQueryProvider>
+    )
+}
+
+export default AccountDetailsWithProviders;
